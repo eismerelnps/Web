57b058ee810b00decae9c925cef31fd345179467
Eismer Lobaina <113050158+eismerelnps@users.noreply.github.com>
2023-02-01 22:48:46 -0500

    Learnig Web

---
+++ idea/variables.js
@@ -0,0 +1,10 @@
+document.writeln("holasss")
+let Name
+let lastName
+let ID
+
+
+document.writeln("Your name is "+`ti `+lastName+ID)
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Hello JavaScript!";
+}
